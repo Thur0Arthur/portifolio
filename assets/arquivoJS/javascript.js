f76ecc0dea6fc495c85638bbc5cc02bc50f692bb
Arthur Francisco <arthur.fr.silvestre@gmail.com>
2023-03-15 23:59:50 -0300

    primeiro commit

---
+++ assets/arquivoJS/javascript.js
@@ -0,0 +1,31 @@
+//AREA DE NAVEGAÇÃO
+
+
+function home() {
+    scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+}
+function aboutMe(){
+    scrollTo({
+        top: 1600,
+        left: 0,
+        behavior: "smooth"
+    })
+}
+function projects(){
+    scrollTo({
+        top: 3200,
+        left: 0,
+        behavior: "smooth"
+    })
+}
+function skills(){
+    scrollTo({
+        top: 6400,
+        left: 0,
+        behavior: "smooth"
+    })
+}
