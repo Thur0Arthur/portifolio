a748a30bc8186d5d02abc9dabf039dc8fc01dd73
Arthur Francisco <arthur.fr.silvestre@gmail.com>
2023-03-19 09:06:18 -0300

    terceiro commit

--- assets/arquivoJS/javascript.js
+++ assets/arquivoJS/javascript.js
@@ -1,31 +1,76 @@
-//AREA DE NAVEGAÇÃO
+//Menu
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
 
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+mobileNavbar.init();
+
+//Menu funções de scroll
 
 function home() {
-    scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
-    })
+  window.scrollTo({
+    top:0,
+    left:0,
+    behavior: 'smooth'
+  })
 }
-function aboutMe(){
-    scrollTo({
-        top: 1600,
-        left: 0,
-        behavior: "smooth"
-    })
+function about() {
+  window.scrollTo({
+    top:1600,
+    left:0,
+    behavior: 'smooth'
+  })
 }
-function projects(){
-    scrollTo({
-        top: 3200,
-        left: 0,
-        behavior: "smooth"
-    })
+function projects() {
+  window.scrollTo({
+    top:3200,
+    left:0,
+    behavior: 'smooth'
+  })
 }
-function skills(){
-    scrollTo({
-        top: 6400,
-        left: 0,
-        behavior: "smooth"
-    })
+function contact() {
+  window.scrollTo({
+    top:6400,
+    left:0,
+    behavior: 'smooth'
+  })
 }
