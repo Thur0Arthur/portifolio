f046df59fc110c8e15023569abbcb18f5aad93fb
Arthur Francisco <arthur.fr.silvestre@gmail.com>
2023-04-04 19:37:05 -0300

    versão 1.0

--- assets/arquivoJS/javascript.js
+++ assets/arquivoJS/javascript.js
@@ -67,10 +67,104 @@ function projects() {
     behavior: 'smooth'
   })
 }
-function contact() {
+function skills() {
   window.scrollTo({
-    top:6400,
+    top:4807,
     left:0,
     behavior: 'smooth'
   })
+}
+
+//Area de sliders
+
+let totalSliders = document.querySelectorAll('.slider-item').length;
+let currentSlide = 0;
+
+document.querySelector('.slider-width').style.width = `calc(100vw * ${totalSliders})`;
+document.querySelector('.slider-controls').style.height = `${document.querySelector('.slider-width').clientHeight}.px`;
+
+function goPrev() {
+  currentSlide--;
+  if(currentSlide < 0) {
+    currentSlide = totalSliders - 1;
+  }
+  updateMargin();
+}
+function goNext() {
+  currentSlide++;
+  if(currentSlide > (totalSliders-1)) {
+    currentSlide = 0;
+  }
+  updateMargin();
+}
+function updateMargin() {
+  let sliderItemWidth = document.querySelector('.slider-item').clientWidth
+  let newMargin = (currentSlide * sliderItemWidth);
+  document.querySelector('.slider-width').style.marginLeft = `-${newMargin}px`;
+
+}
+
+// AREA DAS FUNÇÕES DAS IMGS
+
+let areaTextImg = document.querySelector('.sectionFour-text-click');
+let htmlIMG = document.querySelector('.htmlText');
+let cssImg = document.querySelector('.cssText');
+let jsImg = document.querySelector('.JSText');
+let tsImg = document.querySelector('.TSText');
+let gitImg = document.querySelector('.gitText');
+let bddImg = document.querySelector('.bddText');
+
+function htmlText() {
+  areaTextImg.style.display = "none";
+  htmlIMG.style.display = "block";
+  cssImg.style.display = 'none'
+  jsImg.style.display = "none";
+  tsImg.style.display = "none";
+  gitImg.style.display = "none";
+  bddImg.style.display = "none";
+}
+function cssText() {
+  cssImg.style.display = "block";
+  htmlIMG.style.display = "none";
+  areaTextImg.style.display = "none";
+  jsImg.style.display = "none";
+  tsImg.style.display = "none";
+  gitImg.style.display = "none";
+  bddImg.style.display = "none";
+}
+function JSText() {
+  cssImg.style.display = "none";
+  htmlIMG.style.display = "none";
+  areaTextImg.style.display = "none";
+  jsImg.style.display = "block";
+  tsImg.style.display = "none";
+  gitImg.style.display = "none";
+  bddImg.style.display = "none";
+}
+function TSText() {
+  cssImg.style.display = "none";
+  htmlIMG.style.display = "none";
+  areaTextImg.style.display = "none";
+  jsImg.style.display = "none";
+  tsImg.style.display = "block";
+  gitImg.style.display = "none";
+  bddImg.style.display = "none";
+}
+function gitText() {
+  cssImg.style.display = "none";
+  htmlIMG.style.display = "none";
+  areaTextImg.style.display = "none";
+  jsImg.style.display = "none";
+  tsImg.style.display = "none";
+  gitImg.style.display = "block";
+  bddImg.style.display = "none";
+}
+function bddText() {
+  cssImg.style.display = "none";
+  htmlIMG.style.display = "none";
+  areaTextImg.style.display = "none";
+  jsImg.style.display = "none";
+  tsImg.style.display = "none";
+  gitImg.style.display = "none";
+  bddImg.style.display = "block";
 }
