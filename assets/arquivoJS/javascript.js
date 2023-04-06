66c0d20904ddd04527d2b5fc77d8398bb6ae0321
Arthur Francisco <arthur.fr.silvestre@gmail.com>
2023-04-06 12:23:26 -0300

    commit add clockproject

--- assets/arquivoJS/javascript.js
+++ assets/arquivoJS/javascript.js
@@ -104,6 +104,28 @@ function updateMargin() {
 
 }
 
+//AREA DOS PROJETOS DENTRO DOS SLIDERS
+
+//Projeto RELOGIO
+const hours = document.querySelector('#horas');
+const minutes = document.querySelector('#minutos');
+const seconds = document.querySelector('#segundos');
+
+const clock = setInterval(function time() {
+  let dateToday = new Date();
+  let hr = dateToday.getHours();
+  let min = dateToday.getMinutes();
+  let s = dateToday.getSeconds();
+
+  if(hr <10 )hr = '0'+ hr;
+  if(min <10 )min = '0'+ min;
+  if(s <10 )s = '0'+ s;
+
+  hours.textContent = hr;
+  minutes.textContent = min;
+  seconds.textContent = s;
+});
+
 // AREA DAS FUNÇÕES DAS IMGS
 
 let areaTextImg = document.querySelector('.sectionFour-text-click');
